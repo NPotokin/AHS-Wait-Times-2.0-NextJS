79de85f07b716739d81bdf0c3e5d71af58264f29
NPotokin <npotokin@gmail.com>
2023-10-24 20:35:52 -0600

    started hospital.js files with all the info

---
+++ ahswt2/utils/hospitals.js
@@ -0,0 +1,108 @@
+const Hospitals = [
+    {
+        key: 'caEm1',
+        name: 'Alberta Childrens Hospital',
+        info: 'This facility provides a range of healthcare services for children up to 18 years old.',
+        img: 'https://childrenshospitals.ca/wp-content/uploads/2019/03/achf2.jpg',
+        route: 'albertaChildrensHospital',
+        contactInfo: {
+            adress:{
+                street: '28 Oki Drive NW',
+                city: 'Calgary, Alberta',
+                zip: 'T3B 6A8',
+            },
+            phone: '403-955-7211',
+            website: 'https://www.ahs.ca/ach',
+        },
+        aboutInfo: 'Alberta Childrens Hospital in Calgary partnering together with children, youth, families, staff and physicians to provide the utmost in excellent care. We treat each other with respect, listen to one another, share ideas and information, ask and answer questions, create a safe environment for interaction, and make decisions together.',
+        stat: 'some stats',
+        hourGraph: 'hourly graph',
+        dayGraph: 'day graph',
+        weekGraph: 'week graph'
+    },
+    {
+        key: 'caEm2',
+        name: 'Foothills Medical Centre',
+        info: 'This facility provides a range of healthcare services including a 24/7 emergency department.',
+        img: 'https://i.cbc.ca/1.4868516.1679528199!/fileImage/httpImage/image.JPG_gen/derivatives/16x9_780/foothills-medical-centre-hospital.JPG',
+        route: 'foothillsMedicalCentre',
+        contactInfo: {
+            adress:{
+                street: '1403 29 Street NW',
+                city: 'Calgary, Alberta',
+                zip: 'T2N 2T9',
+            },
+            phone: '403-944-1110',
+            website: 'https://www.ahs.ca/fmc',
+        },
+        aboutInfo: 'The Foothills Medical Centre (FMC), one of Canadas largest hospitals, provides advanced healthcare services to more than two million people.',
+        stat: 'some stats',
+        hourGraph: 'hourly graph',
+        dayGraph: 'day graph',
+        weekGraph: 'week graph'
+    },
+    {
+        key: 'caEm3',
+        name: 'Peter Lougheed Centre',
+        info: 'This facility provides a range of healthcare services including a 24/7 emergency department.',
+        img: 'https://i.cbc.ca/1.5840643.1607971692!/cumulusImage/httpImage/image.jpg_gen/derivatives/16x9_780/peter-lougheed-centre-calgary-drone-shots.jpg',
+        route: 'peterLougheedCentre',
+        contactInfo: {
+            adress:{
+                street: '3500 26 Avenue NE',
+                city: 'Calgary, Alberta',
+                zip: 'T1Y 6J4',
+            },
+            phone: '403-943-4555',
+            website: 'https://www.ahs.ca/plc',
+        },
+        aboutInfo: 'A full-service, acute care hospital offering care to more than 150,000 patients annually. Peter Lougheed Centre is southern Alberta centre of excellence for hematology, respiratory, vascular surgery and addictions and mental health.',
+        stat: 'some stats',
+        hourGraph: 'hourly graph',
+        dayGraph: 'day graph',
+        weekGraph: 'week graph'
+    },
+    {
+        key: 'caEm4',
+        name: 'Rockyview General Hospital',
+        info: 'This facility provides a range of healthcare services including a 24/7 emergency department',
+        img: 'https://i.cbc.ca/1.5840633.1655256865!/cumulusImage/httpImage/image.jpg_gen/derivatives/16x9_780/rockyview-general-hospital-calgary-drone-shots.jpg',
+        route: 'rockyviewGeneralHospital',
+        contactInfo: {
+            adress:{
+                street: '7007 14 Street SW',
+                city: 'Calgary, Alberta',
+                zip: 'T2V 1P9',
+            },
+            phone: '403-943-3000',
+            website: 'https://www.ahs.ca/rgh',
+        },
+        aboutInfo: 'Caring for nearly 220,000 patients every year, Rockyview General Hospital (RGH) offers a full range of acute and outpatient care, including Emergency, Critical Care, Urology and Ophthalmology.',
+        stat: 'some stats',
+        hourGraph: 'hourly graph',
+        dayGraph: 'day graph',
+        weekGraph: 'week graph'
+    },
+    {
+        key: 'caEm5',
+        name: 'South Health Campus',
+        info: 'South Health Campus (SHC) is a full service 272-bed hospital, supporting over 19,000 inpatients per year',
+        img: 'https://kasian.com/wp-content/uploads/2022/08/AB-Health-Services-project.jpg',
+        route: 'southHealthCampus',
+        contactInfo: {
+            adress:{
+                street: '4448 Front Street SE',
+                city: 'Calgary, Alberta',
+                zip: 'T3M 1M4',
+            },
+            phone: '403-956-1111',
+            website: 'https://www.ahs.ca/shc',
+        },
+        aboutInfo: 'The South Health Campus (SHC) is more than a place to treat illness and disease. By inspiring a wellness mindset, we focus on supporting, encouraging and assisting people along their health and wellness journey.',
+        stat: 'some stats',
+        hourGraph: 'hourly graph',
+        dayGraph: 'day graph',
+        weekGraph: 'week graph'
+    },
+]
+export default Hospitals;
