358e547979be1df365d43651b5d7ffba5aae9c1b
NPotokin <npotokin@gmail.com>
2023-10-30 19:40:41 -0600

    rwd for /start page

--- ahswt2/app/page.jsx
+++ ahswt2/app/page.jsx
@@ -4,24 +4,24 @@ import picture from './assets/example.png'
 
 export default function Home() {
   return (
-    <main className='flex flex-col'>
-      <h1 className='mx-auto py-8 text-3xl md:text-5xl lg:text-7xl font-bold text-cyan-900'>
+    <main className='container flex flex-col'>
+      <h1 className='py-8 mx-3 text-center text-3xl md:text-5xl lg:text-7xl font-bold text-cyan-900'>
         Welcome to AHS Wait Times Tracker
       </h1>      
-      <section className='flex flex-row mx-auto max-w-7xl p-2 m-2 py-8 gap-12'>
-        <div className='text-xl md:text-3xl lg:text-5xl w-1/2 text-cyan-600 font-semibold'>
-          The only tool that allows you to track hospitals wait time data for 6 main cities in Alberta.
-        </div>
-        <Image 
+      <div className='py-3 mx-3 text-center text-2xl md:text-4xl lg:text-6xl font-semibold text-cyan-700'>
+        The only tool that allows you to track hospitals wait time data for 6 main cities in Alberta.
+      </div>
+      
+        {/* <Image 
           src={picture} 
           alt=""
-          width={500}
-          height={500} 
-          className='border-2 border-cyan-400 rounded-3xl' />
-      </section>  
+          width={300}
+          height={300} 
+          className='rounded-3xl py-4 my-4 mx-auto' /> */}
+        
       <Link 
         href='/start'
-        className='mx-auto p-3 border-4 border-cyan-500 rounded-3xl font-bold text-2xl text-cyan-500 hover:bg-cyan-200'>
+        className='mx-auto p-3 my-6 border-4 border-cyan-500 rounded-3xl font-bold text-2xl text-cyan-500 hover:bg-cyan-200'>
         Get Started
       </Link> 
         
