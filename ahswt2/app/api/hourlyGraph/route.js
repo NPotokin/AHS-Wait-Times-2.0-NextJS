5a31754af9f79c1e5d4fefc90e800db03a0bd6c7
NPotokin <npotokin@gmail.com>
2023-11-15 13:52:21 -0700

    started setting up first API rout

---
+++ ahswt2/app/api/hourlyGraph/route.js
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function GET(){
+ 
+    try {
+    const currentDate = new Date();
+    const pastDate = new Date(currentDate.getTime() - 60 * 60 * 1000); // Last hour in milliseconds
+
+    const data = await prisma.hospitalTimeStamp.findMany({
+      where: {
+        yearUTC: pastDate.getUTCFullYear(),
+        monthUTC: pastDate.toLocaleString('default', { month: 'short' }),
+        dayUTC: pastDate.getUTCDate(),
+        hourUTC: pastDate.getUTCHours()
+      }
+    });
+
+    return Response.json({data});
+    
+  } catch (error) {
+    return Response.json({ error: `An error ${error} occurred` });
+  }
+}
