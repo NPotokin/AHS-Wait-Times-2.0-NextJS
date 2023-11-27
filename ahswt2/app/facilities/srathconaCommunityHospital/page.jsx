bed397935c38813b7c196442458980e51c67bd65
NPotokin <npotokin@gmail.com>
2023-11-27 15:08:57 -0700

    replaced localhost routes with deployment routes

--- ahswt2/app/facilities/srathconaCommunityHospital/page.jsx
+++ ahswt2/app/facilities/srathconaCommunityHospital/page.jsx
@@ -10,17 +10,17 @@ export default async function srathconaCommunityHospital() {
 const hospital = Hospitals.filter((hospital) => hospital.name === 'Strathcona Community Hospital')[0];
 
 
-const responseHr = await fetch('http://localhost:3000/api/hourlyGraph', {next: { revalidate: 120 }});
+const responseHr = await fetch('https://ahs-wait-times-2-0-next-js-git-main-npotokins-projects.vercel.app/api/hourlyGraph', {next: { revalidate: 120 }});
 const fetchedDataHr = await responseHr.json();
 const filteredDataHr = fetchedDataHr.updatedData.filter((item) => item.slug === 'srathconaCommunityHospital');
 const dataHr = await filteredDataHr;
 
-const responseDay = await fetch('http://localhost:3000/api/dailyGraph', {next: { revalidate: 120 }});
+const responseDay = await fetch('https://ahs-wait-times-2-0-next-js-git-main-npotokins-projects.vercel.app/api/dailyGraph', {next: { revalidate: 120 }});
 const fetchedDataDay = await responseDay.json();
 const filteredDataDay = fetchedDataDay.averageWaitTimes.filter((item) => item.slug === 'srathconaCommunityHospital');
 const dataDay = await filteredDataDay;
 
-const responseWeek = await fetch('http://localhost:3000/api/weeklyGraph', {next: { revalidate: 120 }});
+const responseWeek = await fetch('https://ahs-wait-times-2-0-next-js-git-main-npotokins-projects.vercel.app/api/weeklyGraph', {next: { revalidate: 120 }});
 const fetchedDataWeek = await responseWeek.json();
 const filteredDataWeek = fetchedDataWeek.averageWaitTimes.filter((item) => item.slug === 'srathconaCommunityHospital');
 const dataWeek = await filteredDataWeek;
