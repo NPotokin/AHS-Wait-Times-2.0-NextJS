2d2a8de70cfb6acb8552811d710c7a595b32223d
NPotokin <npotokin@gmail.com>
2023-12-19 12:02:12 -0700

    dynamic force dynamic and revalidate 120 to route handlers

--- ahswt2/app/api/albertaAverages/route.js
+++ ahswt2/app/api/albertaAverages/route.js
@@ -1,7 +1,8 @@
 import prisma from '@/prisma/prismaClient'
 
 
-
+export const dynamic = 'force-dynamic';
+export const revalidate = 120;
 
 export async function GET() {
   try {
--- ahswt2/app/api/dailyGraph/route.js
+++ ahswt2/app/api/dailyGraph/route.js
@@ -1,6 +1,7 @@
 import prisma from '@/prisma/prismaClient'
 
-
+export const dynamic = 'force-dynamic';
+export const revalidate = 120;
 
 export async function GET() {
   try {
