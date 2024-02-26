9c233b6eb2dfa8f140ad77359d640e539dc0705f
NPotokin <npotokin@gmail.com>
2024-02-26 00:48:26 -0700

    trying to increase function runnig time to 20 secundo

--- ahswt2/app/not-found.jsx
+++ ahswt2/app/not-found.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className='container flex flex-col mx-auto pb-10 '>
       <h1 className='md:my-12 lg:my-16 py-8 px-4 my-10 text-center text-3xl lg:text-6xl font-bold text-white bg-cyan-600 rounded-3xl'>
-            Not Found.
+            Not Found...
       </h1>
       <p className='md:my-12 lg:my-16 py-3 mx-3 text-justify text-xl  md:text-3xl lg:text-4xl font-medium text-cyan-700'>
         We are terribly sorry, but it looks like something went wrong. Try again and thank you for your patience! 
